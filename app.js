d490259ade05119825515c05786d8edf5bd9634b
Pragati <pragatibhattad1610@gmail.com>
2020-03-27 16:02:57 +0530

    add redis

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const apiCallFromNode = require('./nodejscall')
-// const redis = require("redis://h:p0251a35af0bf4e3e133ee844da1ab4df203632acde8b0a7e0363c946a7d90002@ec2-52-200-69-76.compute-1.amazonaws.com:10019");
+const redis = require("redis");
 
-// const client = redis.createClient(process.env.REDIS_URL);
+const client = redis.createClient("redis://h:p0251a35af0bf4e3e133ee844da1ab4df203632acde8b0a7e0363c946a7d90002@ec2-52-200-69-76.compute-1.amazonaws.com:10019");
 
 const http = require('http')
 
@@ -10,9 +10,15 @@ http.createServer(async(req, res) => {
             var data = await apiCallFromNode.callApi();
             res.write(JSON.stringify(data));
             res.end();
-            let dataTitle = [];
-            dataTitle = data.find("title");
-            console.log(dataTitle);
+           
+            client.on("error", function(error) {
+                console.error(error);
+              });
+            
+            var dataString = JSON.stringify(data);
+            
+            client.set("key", dataString, redis.print)
+            client.get("key", redis.print);
         }
 }).listen(3000);
 
