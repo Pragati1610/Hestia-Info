abf95d9f61d67924fd35b56bf209f485b2ecbc60
Pragati <pragatibhattad1610@gmail.com>
2020-03-28 20:46:52 +0530

    handle cors

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const http = require("http");
-const app = require("app_express");
+const app = require("./app_express");
 
 const port = process.env.PORT||3000;
 http.createServer(app).listen(port);
