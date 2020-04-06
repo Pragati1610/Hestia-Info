139c1b9ff0d116f522d46996d239d130bd9608a6
Pragati <pragatibhattad1610@gmail.com>
2020-04-06 19:04:12 +0530

    add routes to enable global, country wise stats and global numerical data

--- app_express.js
+++ app_express.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const express = require("express");
 const app = express();
 const router = express.Router();
+const axios = require("axios");
 
 require("dotenv").config();
 const client = redis.createClient(process.env.REDIS_URL);
@@ -11,7 +12,7 @@ const client = redis.createClient(process.env.REDIS_URL);
 
 app.use(cors());
 
-router.get('/', async (req, res) => {
+router.get('/node', async (req, res) => {
     var key = "randomString";
     client.get(key, async function (err, reply) {
         res.setHeader('Content-Type', 'application/json; charset=utf-8');
@@ -30,10 +31,56 @@ router.get('/', async (req, res) => {
             });
         }
     });
-
-
 });
 
-app.use("/node", router);
+router.get("/stats", async (req, res) => {
+    try {
+        const stats = axios.get("https://corona.lmao.ninja/v2/historical/all");
+        const globalData = axios.get("https://corona.lmao.ninja/all");
+        const total = await Promise.all([stats, globalData]);
+
+        return res.status(200).json({
+            time_series: total[0].data,
+            global_stats: total[1].data
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            err: err
+        });
+    }
+});
+
+router.get("/allCountriesData", async (req, res) => {
+    try {
+        const allCountriesData = await axios.get("https://corona.lmao.ninja/countries");
+        return res.status(200).json({
+            allCountriesData: allCountriesData.data
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            err: err
+        });
+    };
+});
+
+
+router.get("/allCountries", async(req,res) => {
+    try{
+        const allCountriesData = await axios.get("https://corona.lmao.ninja/countries");
+        const allCountries = allCountriesData.map(item => item.country);
+        return res.status(200).json({
+            allCountries: allCountries
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            err: err
+        });
+    };
+});
+
+app.use("/", router);
 
 module.exports = app;
